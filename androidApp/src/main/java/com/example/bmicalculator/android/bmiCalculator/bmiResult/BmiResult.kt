38538eaaf2d31eec84a2653db5d0f04e976d7c93
KaKin <lkakin9125@gmail.com>
2024-11-16 10:02:23 +0800

    fixed AnimatedVisibility not show issue

--- androidApp/src/main/java/com/example/bmicalculator/android/bmiCalculator/bmiResult/BmiResult.kt
+++ androidApp/src/main/java/com/example/bmicalculator/android/bmiCalculator/bmiResult/BmiResult.kt
@@ -2,6 +2,8 @@ package com.example.bmicalculator.android.bmiCalculator.bmiResult
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,6 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +32,9 @@ import com.example.bmicalculator.bmiCalculator.model.BmiCategory
 
 @Composable
 fun BmiResult(uiState: BmiResultUiState) {
-    AnimatedVisibility(visible = true) {
+    var visible by remember { mutableStateOf(false) }
+    LaunchedEffect(Unit) { visible = true }
+    AnimatedVisibility(visible = visible, enter = fadeIn()) {
         BmiCardContainer(
             modifier = Modifier
                 .fillMaxWidth()
